99eb1554181374bf39606f00b10a316f4a473128
Bahman Razmpa <razmpa@gmail.com>
2013-06-29 14:25:42 -0700

    Build Skeleton of the Application

--- stockfile/server/FileScanner.java
+++ stockfile/server/FileScanner.java
@@ -4,8 +4,13 @@
  */
 package stockfile.server;
 
+import java.io.File;
+import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
 
 /**
  * Runnable class that controls the OnlineStockInfo object by calling it to
@@ -13,6 +18,20 @@ import java.util.logging.Logger;
  */
 public class FileScanner implements Runnable
 {
+
+    Collection files;
+    File thisDir;
+    String directory = "data/stockfile_repos";
+
+    public void collectFiles()
+    {
+        thisDir = new File(directory);
+        files = FileUtils.listFiles(
+                thisDir,
+                new RegexFileFilter("^(.*?)"),
+                DirectoryFileFilter.DIRECTORY);
+        System.out.println(files);
+    }
 
     /**
      * initialize a thread to update StockList every 120000 ms (2 minutes)
@@ -24,7 +43,9 @@ public class FileScanner implements Runnable
         {
             try
             {
-                Thread.sleep(120000);
+                collectFiles();
+
+                Thread.sleep(1200);
             }
             catch (InterruptedException ex)
             {
