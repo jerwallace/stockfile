2814182b91cdd6a9511002a705c168682a642fa0
Bahman Razmpa <razmpa@gmail.com>
2013-07-13 15:42:45 -0700

    Modify ClientDAO - add "removeClient" and "addClient" methods (Under construction)

--- stockfile/dao/ClientDAO.java
+++ stockfile/dao/ClientDAO.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import stockfile.api.User;
 import stockfile.client.Client;
+import stockfile.client.UserSession;
 
 /**
  *
@@ -25,16 +26,11 @@ public class ClientDAO extends StockFileDAO
         super();
     }
 
-    public void addClient() throws SQLException, SocketException, UnknownHostException
+    public void addClient(Client client) throws SQLException, SocketException, UnknownHostException
     {
 
-        Client client = new Client();
-
-        InetAddress ipAddress = InetAddress.getLocalHost();
-        NetworkInterface nwi = NetworkInterface.getByInetAddress(ipAddress);
-        byte macAddress[] = nwi.getHardwareAddress();
-        System.out.println("IP Address: " + ipAddress.getAddress());
-        System.out.println("MAC Address: " + macAddress);
+//        System.out.println("IP Address: " + ipAddress.getAddress());
+//        System.out.println("MAC Address: " + macAddress);
 
         try
         {
@@ -43,15 +39,15 @@ public class ClientDAO extends StockFileDAO
                     + "user_client (username,client_type,last_sync,ip_address,mac_address) "
                     + "VALUES (?,?,?,?,?);");
 
-            ps.setString(1, UserSession.getInstance().getUserName());
+            ps.setString(1, UserSession.getInstance().getUsername());
             ps.setString(2, client.getType());
-            ps.setString(3, UserSession.getInstance().getLastSync());
-            ps.setBytes(4, ipAddress.getAddress());
-            ps.setBytes(5, macAddress);
+            ps.setTimestamp(3, UserSession.getInstance().getLastSync());
+            ps.setBytes(4, client.getIpAddress());
+            ps.setBytes(5, client.getMacAddress());
 
             int num = ps.executeUpdate();
-            System.out.println(num + " records were added." + ps.toString());
-
+            System.out.println("Client with IP Address: " + client.getIpAddress().toString()
+                    + " and MAC Address: " + client.getMacAddress().toString() + " were added! " + ps.toString());
         }
         catch (SQLException sqlex)
         {
@@ -59,84 +55,47 @@ public class ClientDAO extends StockFileDAO
         }
 
         this.psclose();
-        return 0;
     }
 
-    public void removeClient()
+    public void removeClient(Client client) throws SQLException
     {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public void removeClient()
+        try
+        {
+            ps = conn.prepareStatement("DELETE FROM user_client WHERE username = ? AND mac_address = ?");
+
+            ps.setString(1, UserSession.getInstance().getUsername());
+            ps.setBytes(2, client.getMacAddress());
+
+            ps.executeUpdate();
+        }
+        catch (SQLException sqlex)
+        {
+            throw sqlex;
+        }
+
+        this.psclose();
+    }
+
+    public void updateClient(Client client) throws SQLException
     {
+        try
+        {
+            ps = conn.prepareStatement("UPDATE client "
+                    + "SET client_type = ?, client_desc = ?, client_manufacturer = ?, client_model_no = ? "
+                    + "WHERE username = ? AND mac_address = ? ");
 
-        Client client = new Client();
+            ps.setString(1, client.getType());
+            ps.setString(2, client.getDescription());
+            ps.setString(3, client.getManufacturer());
+            ps.setString(4, client.getModelNo());
 
+            ps.executeUpdate();
 
+        }
+        catch (SQLException sqlex)
+        {
+            throw sqlex;
+        }
         this.psclose();
     }
 
