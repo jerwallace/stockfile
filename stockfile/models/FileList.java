2b04fc40f9a45a95717c21f5f1171bb2bb48e22c
Bahman Razmpa <razmpa@gmail.com>
2013-07-18 18:41:36 -0700

    trying to get filelist back (ignore)

---
+++ stockfile/models/FileList.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockfile.models;
+
+/**
+ *
+ * @author Bahman
+ */
+public class FileList
+{
+}
