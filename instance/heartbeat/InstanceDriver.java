aa6b398db6b1f0129b89e50ebe203d5bbc428e28
Jeremy Wallace <jeremy@westendmedia.ca>
2013-07-21 20:11:03 -0700

    Added Jars

---
+++ instance/heartbeat/InstanceDriver.java
@@ -0,0 +1,74 @@
+package instance.heartbeat;
+
+import stockfile.PeriodicSync;
+import stockfile.controllers.ServerController;
+import stockfile.controllers.StateController;
+import stockfile.exceptions.ApplicationFailedException;
+import stockfile.security.StockFileSession;
+
+/**
+ * Stockfile driver is the main class for the StockFile application.
+ * @author jeremywallace
+ * @project StockFile, CICS 525
+ * @organization University of British Columbia
+ * @date July 20, 2013
+ */
+public class InstanceDriver
+{
+	private static final int SYNC_DELAY = 10000;
+
+	/**
+	 * Initializes the application by requiring the user to login and then loading the previous state
+	 * from the saved .pbj file in the users default directory.
+	 */
+    public InstanceDriver() throws Exception
+    {
+        try {
+        	ServerController.getInstance().run();
+            StateController.getInstance().loadState();
+            StateController.getInstance().loadDirectoryState(StockFileSession.getInstance().getCurrentClient().getFullDir());
+        } catch (ApplicationFailedException ex) {
+            System.err.println(ex);
+            System.exit(0);
+        } 
+        
+    }
+    
+    /**
+     * The main class launches the stockfile application.
+     * Shutdown hooks are established to save the state of the application.
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception
+    {
+
+        InstanceDriver stockfileInstance = new InstanceDriver();
+
+        //Attach shutDownhook for data persistence after shutDown
+        stockfileInstance.attachShutDownHook();
+        
+        System.out.println("Initializing periodic sync...");
+        Thread periodicSync = new Thread(new PeriodicSync(SYNC_DELAY));
+        periodicSync.start();
+        
+    }
+
+    /**
+     * Public method that creates a Runtime ShutDownHook thread to maintain the
+     * State object values in case of system shut down to allow for data
+     * persistence
+     */
+    public void attachShutDownHook()
+    {
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            @Override
+            public void run()
+            {
+            	System.out.println("Saving state... Goodbye!");
+            	StateController.getInstance().saveState();
+            }
+        });
+    }
+}
---
+++ instance/heartbeat/InstanceHBThread.java
@@ -0,0 +1,58 @@
+package instance.heartbeat;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * Class InstanceHBThread extends Thread. This thread opens a UDP socket and
+ * listens to heartbeat messages from the Gateway. Once it receives a heartbeat
+ * it immediately responds back to the Gateway to indicate that its alive.
+ * <p/>
+ * @author Bahman
+ */
+public class InstanceHBThread extends Thread {
+
+    protected static boolean alive = true;
+    private int receivePortNumber;
+
+    //Public constructor for the InstanceHBThread that takes in the listening port
+    public InstanceHBThread(int receivePort) {
+        super();
+        this.receivePortNumber = receivePort;
+    }
+
+    @Override
+    public void run() {
+        
+        try {
+            //Create a UDP socket on the pre-specified port to listen for heartbeat
+            DatagramSocket socket = new DatagramSocket(this.receivePortNumber);
+
+            System.out.print("Heartbeat send/receive status->");
+
+            while (alive) {
+                byte[] buf = new byte[256];
+
+                //Create receive datagram and read it from socket when available
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+
+                String response = "heartbeat";
+                buf = response.getBytes();
+
+                //create response packet to Gateway using the receive packet's IP address and Port
+                packet = new DatagramPacket(buf, buf.length, packet.getAddress(), packet.getPort());
+
+                //send response packet
+                socket.send(packet);
+
+                System.out.println(" <3 ");
+            }
+
+            //If failure, close the UDP socket
+            socket.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
